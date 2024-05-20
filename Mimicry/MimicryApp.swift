7d3db7002d55a499b0e1c9c99424da855f387095
Afif Fadhlurrahman <afif.fadhlurrahman2@gmail.com>
2024-05-20 10:52:21 +0700

    Initial Commit

---
+++ Mimicry/MimicryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimicryApp.swift
+//  Mimicry
+//
+//  Created by Muhammad Afif Fadhlurrahman on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MimicryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
